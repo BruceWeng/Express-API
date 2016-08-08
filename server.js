65eeb1c2cc2e3cf96c34d169dea363fa4737f99c
BruceWeng <weng8916@gmail.com>
2016-08-08 15:21:16 -0700

    Serialize array to JSON

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', function(request, response) {
   response.send('Hello World');
 });
 
+app.get('/blocks', function(request, response) {
+  let blocks = ['Fixed', 'Movable', 'Rotating'];
+  response.json(blocks);
+});
+
 app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
