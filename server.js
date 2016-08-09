e215dafbb6d9a8f1efdad46b8dcba0b8798dc36c
BruceWeng <weng8916@gmail.com>
2016-08-08 17:09:57 -0700

    use custom middleware logger

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 'use strict';
 let express = require('express');
 let app = express();
-
+let logger = require('./logger');
 // app.get('/', function(request, response) {
 //   response.sendFile(__dirname + '/public/index.html');
 // });
-
+app.use(logger);
 app.use(express.static('public'));
 
-app.get('/blocks', function(request, response) {
+app.get('/blocks', function(req, res) {
   let blocks = ['Fixed', 'Movable', 'Rotating'];
-  response.redirect(301, '/parts');
+  res.json(blocks);
 });
 
-app.get('/parts', function(request, response) {
+app.get('/redirect', function(req, res) {
+  res.redirect(301, '/parts');
+})
+app.get('/parts', function(req, res) {
   console.log('This is /parts');
 });
 
