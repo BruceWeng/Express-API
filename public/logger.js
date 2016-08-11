3e253313d668b0e732bf93fabf3c06a4fa61a76b
BruceWeng <weng8916@gmail.com>
2016-08-11 12:05:33 -0700

    Create Hello World Component and transpile into public

---
+++ public/logger.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (req, res, next) {
+  var start = +new Date();
+  var stream = process.stdout;
+  var url = req.url;
+  var method = req.method;
+
+  res.on('finish', function () {
+    var duration = +new Date() - start;
+    var message = method + ' to ' + url + ' \ntook ' + duration + ' ms \n\n';
+    stream.write(message);
+  });
+  next();
+};
