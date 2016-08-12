5d623639f3c1f60fac6c718fca51ac2f74a6e98f
BruceWeng <weng8916@gmail.com>
2016-08-11 17:48:25 -0700

    Create Comment and CommentBox components

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentBox from './components/CommentBox';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello React</h1>
+        <CommentBox />
       </div>
     );
   }
---
+++ src/components/Comment.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Comment extends React.Component {
+  render() {
+    return (
+      <div className="comment">
+        <p className="comment-header">Anne Droid</p>
+        <p className="comment-body">
+          I wanna know what love is...
+        </p>
+        <div className="comment-footer">
+          <a href="#" className="comment-footer-delete">
+            Delete comment
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Comment;
